104a94acd4730cd6bb37ab6f4ece075b00ecb435
水月 <sun.melodies@gmail.com>
2020-12-01 20:59:47 +0800

    rename 0.3.0a2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open("README.md", "rt", encoding="utf8") as f:
 
 setuptools.setup(
     name='Lin-CMS',
-    version="0.3.0a1",
+    version="0.3.0a2",
     url='https://pypi.org/project/Lin-CMS/',
     license='MIT',
     author='pedroGao',
